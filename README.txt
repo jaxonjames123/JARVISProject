120e95834af9d0425d6d0bd1b908e2309bae4462
Jaxon Terrell <terrellj8@students.rowan.edu>
2020-05-24 23:09:42 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+To run file:
+
+Download python here: https://www.python.org/downloads/
+Must have a Wolfrom Alpha account made, and you must use your own unique APPID made,
+    which you can do at https://developer.wolframalpha.com/
+    Use this unique APPID in place of the one I used for the client
+
+
+1) Install Python
+2) pip install wolframalpha
+3) pip install pysimplegui
+4) pip install wikipedia
+5) pip install pyttsx3==2.6
+6) pip install SpeechRecognition
+7) pip install PyAudio
+6) python jarvis.py
--- jarvis.py
+++ jarvis.py
@@ -2,15 +2,19 @@ import wolframalpha
 import PySimpleGUI as sg
 import wikipedia
 import pyttsx3
+import speech_recognition as sr
 
 client = wolframalpha.Client('4UW6LT-6757UHW5GY')
 engine = pyttsx3.init()
+r = sr.Recognizer()
 
 sg.theme('DarkBlue')  # Add a touch of color
-# All the stuff inside your window.
-layout = [[sg.Text('Hello, I am JARVIS')],
-          [sg.Text('Please enter what you would like to find out:'), sg.InputText()],
-          [sg.Button('Ok'), sg.Button('Cancel')]]
+with sr.Microphone() as source:
+    # All the stuff inside your window.
+    layout = [[sg.Text('Hello, I am JARVIS, please ask me a question')],
+              [sg.Button('Ok'), sg.Button('Cancel')]]
+    print("Please talk")
+    audio = r.listen(source)
 
 # Create the Window
 window = sg.Window('JARVIS Supercomputer', layout)
@@ -21,22 +25,28 @@ while True:
     if event in (None, 'Cancel'):  # if user closes window or clicks cancel
         break
     try:
-        wiki_res = wikipedia.summary(values[0], sentences=2)
-        wolfram_res = next(client.query(values[0]).results).text
-        engine.say(wolfram_res)
-        sg.PopupNonBlocking("Wolfram Result: " + wolfram_res, "Wikipedia Result: " + wiki_res)
-    except wikipedia.exceptions.DisambiguationError:
-        wolfram_res = next(client.query(values[0]).results).text
-        engine.say(wolfram_res)
-        sg.PopupNonBlocking(wolfram_res)
-    except wikipedia.exceptions.PageError:
-        wolfram_res = next(client.query(values[0]).results).text
-        engine.say(wolfram_res)
-        sg.PopupNonBlocking(wolfram_res)
-    except:
-        wiki_res = wikipedia.summary(values[0], sentences=2)
-        engine.say(wiki_res)
-        sg.PopupNonBlocking(wiki_res)
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+        try:
+            wiki_res = wikipedia.summary(r.recognize_google(audio), sentences=2)
+            wolfram_res = next(client.query(r.recognize_google(audio)).results).text
+            engine.say(wolfram_res)
+            sg.PopupNonBlocking("Wolfram Result: " + wolfram_res, "Wikipedia Result: " + wiki_res)
+        except wikipedia.exceptions.DisambiguationError:
+            wolfram_res = next(client.query(r.recognize_google(audio)).results).text
+            engine.say(wolfram_res)
+            sg.PopupNonBlocking(wolfram_res)
+        except wikipedia.exceptions.PageError:
+            wolfram_res = next(client.query(r.recognize_google(audio)).results).text
+            engine.say(wolfram_res)
+            sg.PopupNonBlocking(wolfram_res)
+        except:
+            wiki_res = wikipedia.summary(r.recognize_google(audio), sentences=2)
+            engine.say(wiki_res)
+            sg.PopupNonBlocking(wiki_res)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request respyults from Google Speech Recognition service; {0}".format(e))
 
     engine.runAndWait()
 
