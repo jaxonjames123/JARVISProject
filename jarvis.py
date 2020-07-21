e9b265c8f46c7c9e87efa1439db9ecafc5c21cb7
Jaxon Terrell <terrellj8@students.rowan.edu>
2020-07-21 15:04:33 -0400

    Add files via upload

--- jarvis.py
+++ jarvis.py
@@ -4,7 +4,7 @@ import wikipedia
 import pyttsx3
 import speech_recognition as sr
 
-client = wolframalpha.Client('4UW6LT-6757UHW5GY')
+client = wolframalpha.Client('4UW6LT-A3WR96V43Q')
 engine = pyttsx3.init()
 r = sr.Recognizer()
 
@@ -13,7 +13,6 @@ sg.theme('DarkBlue')  # Add a touch of color
 # All the stuff inside your window.
 layout = [[sg.Text('Hello, I am JARVIS, please ask me a question')],
           [sg.Button('Type a question'), sg.Button('Ask a question'), sg.Button('Cancel')]]
-
 
 # Create the Window
 window = sg.Window('JARVIS Supercomputer', layout)
@@ -35,41 +34,66 @@ while True:
                 wolfram_res = next(client.query(r.recognize_google(audio)).results).text
                 engine.say(wolfram_res)
                 sg.PopupNonBlocking("Wolfram Result: " + wolfram_res, "Wikipedia Result: " + wiki_res)
+                if event in (None, 'Cancel'):  # if user closes window or clicks cancel
+                    break
             except wikipedia.exceptions.DisambiguationError:
                 wolfram_res = next(client.query(r.recognize_google(audio)).results).text
                 engine.say(wolfram_res)
                 sg.PopupNonBlocking(wolfram_res)
+                if event in (None, 'Cancel'):  # if user closes window or clicks cancel
+                    break
             except wikipedia.exceptions.PageError:
                 wolfram_res = next(client.query(r.recognize_google(audio)).results).text
                 engine.say(wolfram_res)
                 sg.PopupNonBlocking(wolfram_res)
+                if event in (None, 'Cancel'):  # if user closes window or clicks cancel
+                    break
             except:
                 wiki_res = wikipedia.summary(r.recognize_google(audio), sentences=2)
                 engine.say(wiki_res)
                 sg.PopupNonBlocking(wiki_res)
+                if event in (None, 'Cancel'):  # if user closes window or clicks cancel
+                    break
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
-            print("Could not request respyults from Google Speech Recognition service; {0}".format(e))
-    # Write code for gui to allow popup window to ask a question by being typed
-    # if event == 'Type a question':
-    #     try:
-    #         wiki_res = wikipedia.summary(values[0], sentences=2)
-    #         wolfram_res = next(client.query(values[0]).results).text
-    #         engine.say(wolfram_res)
-    #         sg.PopupNonBlocking("Wolfram Result: " + wolfram_res, "Wikipedia Result: " + wiki_res)
-    #     except wikipedia.exceptions.DisambiguationError:
-    #         wolfram_res = next(client.query(values[0]).results).text
-    #         engine.say(wolfram_res)
-    #         sg.PopupNonBlocking(wolfram_res)
-    #     except wikipedia.exceptions.PageError:
-    #         wolfram_res = next(client.query(values[0]).results).text
-    #         engine.say(wolfram_res)
-    #         sg.PopupNonBlocking(wolfram_res)
-    #     except:
-    #         wiki_res = wikipedia.summary(values[0], sentences=2)
-    #         engine.say(wiki_res)
-    #         sg.PopupNonBlocking(wiki_res)
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    # Logic allows user to type out a question
+    if event == 'Type a question':
+        layout = [[sg.Text('Please type your question below')],
+                  [sg.InputText()],
+                  [sg.Button('Ok'), sg.Button('Cancel')]]
+        window2 = sg.Window('JARVIS Supercomputer', layout)
+        while True:
+            event, values = window2.read()
+            if event in (None, 'Cancel'):  # if user closes window or clicks cancel
+                break
+            try:
+                wiki_res = wikipedia.summary(values[0], sentences=2)
+                wolfram_res = next(client.query(values[0]).results).text
+                engine.say(wolfram_res)
+                sg.PopupNonBlocking("Wolfram Result: " + wolfram_res, "Wikipedia Result: " + wiki_res)
+                if event in (None, 'Cancel'):  # if user closes window or clicks cancel
+                    break
+            except wikipedia.exceptions.DisambiguationError:
+                wolfram_res = next(client.query(values[0]).results).text
+                engine.say(wolfram_res)
+                sg.PopupNonBlocking(wolfram_res)
+                if event in (None, 'Cancel'):  # if user closes window or clicks cancel
+                    break
+            except wikipedia.exceptions.PageError:
+                wolfram_res = next(client.query(values[0]).results).text
+                engine.say(wolfram_res)
+                sg.PopupNonBlocking(wolfram_res)
+                if event in (None, 'Cancel'):  # if user closes window or clicks cancel
+                    break
+            except:
+                wiki_res = wikipedia.summary(values[0], sentences=2)
+                engine.say(wiki_res)
+                sg.PopupNonBlocking(wiki_res)
+                if event in (None, 'Cancel'):  # if user closes window or clicks cancel
+                    break
 
     engine.runAndWait()
 
